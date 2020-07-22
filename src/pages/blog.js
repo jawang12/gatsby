4ea97d695e4a845a740218c3312e5b7a08d76145
Jimmy Wang <jawang12@gmail.com>
2020-07-22 02:14:30 -0400

    blog setup

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 import Layout from '../components/layout';
 
 const Blog = () => {
+  const markdown = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              date
+              title
+            }
+          }
+        }
+      }
+    }
+  `);
   return (
     <Layout>
       <div>
         <h1>Blog</h1>
         <p>Posts: </p>
+        <ol>
+          {markdown.allMarkdownRemark.edges.map((post, i) => (
+            <li key={i}>
+              <h2>{post.node.frontmatter.title}</h2>
+              <p>{post.node.frontmatter.date}</p>
+            </li>
+          ))}
+        </ol>
       </div>
     </Layout>
   );
